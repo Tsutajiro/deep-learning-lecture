350f603b5dfbbba14d1c326e1de9ea3358432607
tsutaj <tsutajiro@gmail.com>
2018-11-09 10:33:15 +0900

    modified 453_train_neuralnet.py to output intermediate result to file

--- code/453_train_neuralnet.py
+++ code/453_train_neuralnet.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import sys
 from dataset.mnist import load_mnist
 from lib.TwoLayerNet import TwoLayerNet
 
@@ -9,6 +10,7 @@ from lib.TwoLayerNet import TwoLayerNet
 #  @date        November 8, 2018
 
 def main():
+    output_filename = '453_result.out'
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
     iters_num     = 10000
@@ -23,26 +25,27 @@ def main():
 
     network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-    for i in range(iters_num):
-        print('iter {} ...'.format(i))
-        batch_mask = np.random.choice(train_size, batch_size)
-        x_batch = x_train[batch_mask]
-        t_batch = t_train[batch_mask]
+    with open(output_filename, 'w') as fp:
+        for i in range(iters_num):
+            print('iter {} ...'.format(i), file=sys.stderr)
+            batch_mask = np.random.choice(train_size, batch_size)
+            x_batch = x_train[batch_mask]
+            t_batch = t_train[batch_mask]
+            
+            grad = network.numerical_gradient(x_batch, t_batch)
+            
+            for key in ('W1', 'b1', 'W2', 'b2'):
+                network.params[key] -= learning_rate * grad[key]
 
-        grad = network.numerical_gradient(x_batch, t_batch)
+                loss = network.loss(x_batch, t_batch)
+                train_loss_list.append(loss)
 
-        for key in ('W1', 'b1', 'W2', 'b2'):
-            network.params[key] -= learning_rate * grad[key]
-
-        loss = network.loss(x_batch, t_batch)
-        train_loss_list.append(loss)
-
-        if i % iter_per_epoch == 0:
-            train_acc = network.accuracy(x_train, t_train)
-            test_acc  = network.accuracy(x_test , t_test )
-            train_acc_list.append(train_acc)
-            test_acc_list.append(test_acc)
-            print('train acc, test acc | {0}, {1}'.format(train_acc, test_acc))
+            if i % iter_per_epoch == 0:
+                train_acc = network.accuracy(x_train, t_train)
+                test_acc  = network.accuracy(x_test , t_test )
+                train_acc_list.append(train_acc)
+                test_acc_list.append(test_acc)
+                fp.write('train acc, test acc | {0}, {1}\n'.format(train_acc, test_acc))
 
 if __name__ == '__main__':
     main()
